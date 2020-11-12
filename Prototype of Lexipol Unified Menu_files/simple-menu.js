8dfe7860a8a8c288de12439cf3adf8e5d9d8f93c
Ken Shen Robinson <ken@thekenshen.com>
2020-11-12 22:18:06 +0800

    Interactive Mockup for Menu Unification Concept
    Please do not even look at the code behind this. For visual purposes only.

---
+++ Prototype of Lexipol Unified Menu_files/simple-menu.js
@@ -0,0 +1,17 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function mobileMenuOpen() {
+    document.getElementById("gmDropdown").classList.toggle("show");
+}
+
+function mobileMenuOpenEvents() {
+    document.getElementById("gmDropdownEvents").classList.toggle("show");
+}
+
+function mobileMenuOpenDashboard() {
+    document.getElementById("gmDropdownDashboard").classList.toggle("show");
+}
+
+function mobileMenuOpenAdminlearning() {
+    document.getElementById("gmDropdownAdminlearning").classList.toggle("show");
+}
